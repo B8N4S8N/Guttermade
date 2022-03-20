6f5b3af6e045953c51e092ff32bf705bfcf05828
daoleno <daodaoonline@gmail.com>
2022-03-20 13:48:30 +0800

    Landing page.

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,21 +1,313 @@
-import Head from "next/head";
-import Image from "next/image";
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import {
+  MenuIcon,
+  IdentificationIcon,
+  XIcon,
+  HeartIcon,
+} from "@heroicons/react/outline";
+import { ArchiveIcon, ChatAltIcon, ChevronRightIcon, CodeIcon, CollectionIcon } from "@heroicons/react/solid";
+import { appServer } from "config";
+import { demoServer } from "config";
+
+const navigation = [
+  // {
+  //   name: "Twitter",
+  //   href: "#",
+  //   icon: (props) => (
+  //     <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+  //       <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+  //     </svg>
+  //   ),
+  // },
+  {
+    name: "Discord",
+    href: "#",
+    icon: (props: any) => (
+      <svg
+        fill="currentColor"
+        viewBox="0 0 127.14 96.36"
+        {...props}
+      >
+        <path d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z" />
+      </svg>
+    ),
+  },
+  {
+    name: "GitHub",
+    href: "#",
+    icon: (props: any) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
+const features = [
+  {
+    name: "Profile NFT",
+    description:
+      "Create profile NFT that gives you control of your content.",
+    icon: IdentificationIcon,
+  },
+  {
+    name: "Publication",
+    description: "Publications come in three primary types: posts, comments, and mirrors.",
+    icon: ChatAltIcon,
+  },
+  {
+    name: "Collect",
+    description:
+      "Like what you see? Collect posts from people you follow.",
+    icon: CollectionIcon,
+  },
+  {
+    name: "Follow",
+    description: "When you follow a profile, you will be given a Follow NFT, ",
+    icon: HeartIcon,
+  },
+  {
+    name: "IPFS",
+    description: "Your content is stored on IPFS, so it can be permanently stored.",
+    icon: ArchiveIcon,
+  },
+  {
+    name: "Open Source",
+    description:
+      "The source code is available on Github, everyone can contribute to the project.",
+    icon: CodeIcon,
+  },
+];
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-black">
-      <Head>
-        <title>Platforms on Vercel</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="bg-gray-900">
+      <div className="relative overflow-hidden">
+        <Popover as="header" className="relative">
+          <div className="bg-gray-900 pt-6">
+            <nav
+              className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-1">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="#">
+                    <span className="block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400  uppercase font-extrabold">
+                      punk3
+                    </span>
+                  </a>
+                  <div className="-mr-2 flex items-center md:hidden">
+                    <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:flex md:items-center md:space-x-6">
+                <a
+                  href={appServer}
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-base rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                >
+                  Launch App
+                </a>
+              </div>
+            </nav>
+          </div>
 
-      <div className="m-auto w-48">
-        <Image
-          width={512}
-          height={512}
-          src="/logo.png"
-          alt="Platforms on Vercel"
-        />
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+            >
+              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="px-5 pt-4 flex items-center justify-between">
+                  <a href="#">
+                    <span className="block bg-clip-text text-transparent bg-gradient-to-r from-teal-500 to-cyan-400  uppercase font-extrabold">
+                      Punk3
+                    </span>
+                  </a>
+                  <div className="-mr-2">
+                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="h-6 w-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="pt-5 pb-6">
+                  <div className="px-2 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="mt-6 px-5">
+                    <a
+                      href={appServer}
+                      className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                    >
+                      Launch App
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+        <main>
+          <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+            <div className="mx-auto max-w-7xl lg:px-8">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                  <div className="lg:py-24">
+                    <a
+                      href="https://lens.dev/"
+                      target="_blank"
+                      className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
+                    >
+                      <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-gradient-to-r from-teal-500 to-cyan-600 rounded-full">
+                        Early Access
+                      </span>
+                      <span className="ml-4 text-sm">With Lens Protocol</span>
+                      <ChevronRightIcon
+                        className="ml-2 w-5 h-5 text-gray-500"
+                        aria-hidden="true"
+                      />
+                    </a>
+                    <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                      <span className="block">Web3 blogging platform for</span>
+                      <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
+                        crypto punks
+                      </span>
+                    </h1>
+                    <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+                      Building the next generation web3 social blogging network for crypto-punks with NFTs.
+                    </p>
+                    <div className="mt-10 sm:mt-12">
+                      <div className="sm:flex">
+                        <div className="mt-3 sm:mt-0 sm:ml-3">
+                          <a
+                            href={demoServer}
+                            target="_blank"
+                            className="block w-full py-3 px-4 rounded-md shadow  text-white font-medium hover:from-teal-600 border border-cyan-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                          >
+                            Demo
+                          </a>
+                        </div>
+                        <div className="mt-3 sm:mt-0 sm:ml-3">
+                          <a
+                            href={appServer}
+                            className="block w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                          >
+                            Launch App
+                          </a>
+                        </div>
+                      </div>
+                      <p className="mt-3 text-sm text-gray-300 sm:mt-4">
+                        <a href="#" className="font-medium text-white">
+                          Beta version {" "}
+                        </a>
+                        for hobby punks. Data will be lost when we switch to mainnet.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+                  <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                    {/* Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ */}
+                    <img
+                      className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                      src="https://tailwindui.com/img/component-images/cloud-illustration-teal-cyan.svg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+
+        {/* Feature section with grid */}
+
+        <div className="relative bg-white py-16 sm:py-24 lg:py-32">
+          <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+            <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">
+              Build happier
+            </h2>
+
+            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+              Everything you need to build your web3 blog
+            </p>
+            <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+              We have a lot of great features integrated with the lens protocol
+            </p>
+            <div className="mt-12">
+              <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                {features.map((feature) => (
+                  <div key={feature.name} className="pt-6">
+                    <div className="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                      <div className="-mt-6">
+                        <div>
+                          <span className="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                            <feature.icon
+                              className="h-6 w-6 text-white"
+                              aria-hidden="true"
+                            />
+                          </span>
+                        </div>
+                        <h3 className="mt-8 text-lg font-medium text-gray-900 tracking-tight">
+                          {feature.name}
+                        </h3>
+                        <p className="mt-5 text-base text-gray-500">
+                          {feature.description}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <footer className="bg-gray-900">
+          <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+            <div className="flex justify-center space-x-6 md:order-2">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-gray-400 hover:text-gray-500"
+                >
+                  <span className="sr-only">{item.name}</span>
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
+                </a>
+              ))}
+            </div>
+            <div className="flex justify-center">
+              <p className="text-center text-base text-gray-400">
+                &copy; 2022 Punk3Lab
+              </p>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   );
